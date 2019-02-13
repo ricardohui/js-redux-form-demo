aae24df704339843dfe0b732bc85b82cd742bfe5
Ricardo <eehaha@gmail.com>
2019-02-14 07:53:44 +0800

    initialise value for redux form

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,27 +1,49 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import * as actions from "../actions";
+import TextField from "@material-ui/core/TextField";
 import { connect } from "react-redux";
+import { Field, reduxForm, initialize } from "redux-form";
+import * as actions from "../actions";
 class Detail extends React.Component {
   componentDidMount() {
     this.props.fetchApps();
+    initialize("appForm", { lastName: "Chan" });
+  }
+  renderTitleField = field => {
+    // README: outside the return, you need to use field.input.name
+    const elementID = `input-${field.input.name}`;
+    return <TextField id={elementID} name={field.name} {...field.input} />;
+  };
+  onSubmit(formValues) {
+    this.props.submitForm(formValues);
+    // console.log(formValues);
+    // this.props.createPost(values);
   }
   renderThisApplication = (applications, identifier) => {
-    if (applications.length > 1) {
-      const applicationObj = {};
-      applications.forEach(app => {
-        applicationObj[app.id] = app;
-      });
-      return (
-        <div>
-          {applicationObj[identifier].lastName},
-          {applicationObj[identifier].loanAmount},
-          {applicationObj[identifier].email},{applicationObj[identifier].tenure}
-        </div>
-      );
-    } else {
-      return null;
-    }
+    const { handleSubmit } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <Field
+            label="Family Name"
+            name="lastName"
+            component={this.renderTitleField}
+          />
+          <Field
+            label="Loan Amount"
+            name="loanAmount"
+            component={this.renderTitleField}
+          />
+          <Field label="Email" name="email" component={this.renderTitleField} />
+          <Field
+            label="Tenure"
+            name="tenure"
+            component={this.renderTitleField}
+          />
+        </form>
+      </div>
+    );
   };
 
   render() {
@@ -42,10 +64,34 @@ class Detail extends React.Component {
   }
 }
 
-function mapStateToProps(state) {
-  return { applications: state.applications };
+function mapStateToProps(state, ownProps) {
+  const { applications } = state;
+  if (applications.length > 1) {
+    const applicationObj = {};
+    applications.forEach(app => {
+      applicationObj["" + app.id] = app;
+      // debugger;
+    });
+    return {
+      applications: state.applications,
+      initialValues: {
+        lastName: applicationObj[ownProps.match.params.id].lastName,
+        loanAmount: applicationObj[ownProps.match.params.id].loanAmount,
+        tenure: applicationObj[ownProps.match.params.id].tenure,
+        email: applicationObj[ownProps.match.params.id].email
+      }
+    };
+  }
+  return {
+    applications: state.applications
+  };
 }
+const wrappedForm = reduxForm({
+  form: "appForm",
+  enableReinitialize: true
+})(Detail);
+
 export default connect(
   mapStateToProps,
   actions
-)(Detail);
+)(wrappedForm);
