ef0af53998a5f164255528f80d21f872b172b6c5
Ricardo <eehaha@gmail.com>
2019-02-14 06:22:13 +0800

    add footer and header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div>Created by me</div>;
 };
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <span>
+        <Link to="/apply">Apply Loan</Link>
+      </span>
+      <span>, </span>
+      <span>
+        <Link to="/applications">View Existing Loan Applications</Link>
+      </span>
+    </div>
+  );
 };
 export default Header;
