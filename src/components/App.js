c94802ebfd3a6fbfff50f6babada6cabd01cf986
Ricardo <eehaha@gmail.com>
2019-02-14 06:23:48 +0800

    Add routes to Home

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,15 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Form from "./Form";
 import List from "./List";
+import Home from "./Home";
 import Detail from "./Detail";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/home" component={Home} />
         <Route path="/apply" render={() => <Form />} />
         <Route path="/applications" exact render={() => <List />} />
         <Route
