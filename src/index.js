54701ae20b94aa98641e92f221b959aadfbbbde4
Ricardo <eehaha@gmail.com>
2019-02-14 06:20:33 +0800

    add redux thunk

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
