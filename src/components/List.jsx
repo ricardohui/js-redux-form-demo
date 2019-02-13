184fc4e5c80b8549ce6cd023ce5b6cc7f4804fb6
Ricardo <eehaha@gmail.com>
2019-02-14 06:23:35 +0800

    implement List

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,6 +1,43 @@
 import React from "react";
+import { fetchApps } from "../actions";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+class List extends React.Component {
+  componentDidMount() {
+    this.props.fetchApps();
+  }
+  renderApplications = () => {
+    const { applications } = this.props;
+    if (applications.length > 0) {
+      return applications.map(app => {
+        return (
+          <li key={app.email}>
+            <Link to={"/applications/" + app.id}>{`${app.lastName} (email: ${
+              app.email
+            }) applied a loan for $${app.loanAmount} and promised to repay in ${
+              app.tenure
+            } months. `}</Link>
+            {app.status.toUpperCase()}
+          </li>
+        );
+      });
+    }
+    return <div />;
+  };
+  render() {
+    return (
+      <div>
+        <h1>List of Loan Applications</h1>
+        <ol>{this.renderApplications()}</ol>
+      </div>
+    );
+  }
+}
 
-const List = () => {
-  return <div>List</div>;
-};
-export default List;
+function mapStateToProps(state) {
+  return { applications: state.applications };
+}
+export default connect(
+  mapStateToProps,
+  { fetchApps }
+)(List);
