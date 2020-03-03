4df0aa62eecacd25a1538dcdc5cda1d1a2735fd2
Ricardo Hui <rcdhui@gmail.com>
2020-03-03 17:57:44 +0800

    only two links in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,41 +32,16 @@ const Header = () => {
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div className="text-sm lg:flex-grow">
           <Link
-            to="/"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white  mr-4 "
-          >
-            Enquries
-          </Link>
-          <Link
-            to="/"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white  mr-4 "
-          >
-            Applications
-          </Link>
-
-          <Link
-            to="/loans"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white   mr-4"
-          >
-            Loans
-          </Link>
-          <Link
-            to="/"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white   mr-4"
-          >
-            Agents
-          </Link>
-          <Link
-            to="/"
-            className="block mt-4 lg:inline-block lg:mt-0 text-white   mr-4"
-          >
-            Permission Control
-          </Link>
-          <Link
             to="/batch_valuation"
             className="block mt-4 lg:inline-block lg:mt-0 text-white  mr-4"
           >
-            Batch Valuation
+            Sending Monthly Valuation Request
+          </Link>
+          <Link
+            to="/batch_valuation/asss"
+            className="block mt-4 lg:inline-block lg:mt-0 text-white  mr-4"
+          >
+            Filling Monthly Valuation Form as Surveyor
           </Link>
         </div>
       </div>
